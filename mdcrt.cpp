e12039cfdae6e282c0742fa6bffbc6e5914f62b4
HybridDog <adresse_test@t-online.de>
2015-01-31 11:20:09 +0100

    mnnmm?

--- mdcrt.cpp
+++ mdcrt.cpp
@@ -21,7 +21,11 @@ int main() {
 	if (owner == "n")
 		owner = "HybridDog";
 
+	if (screenshot_link == "n")
+		screenshot_link = "http://bit.ly/1wOCWpq";
+
 	string giturl = "[url=https://github.com/" + owner + "/" + modname;
+	string raw_giturl = "[url=https://raw.githubusercontent.com/" + owner + "/" + modname + "/master/";
 	string ziplink = giturl + "/archive/master.zip]zip[/url]";
 	string tarlink = giturl + "/archive/master.tar.gz]tar.gz[/url]";
 
@@ -30,8 +34,8 @@ int main() {
 
 	cout << "[Mod] " << modname << " [" << modname << "]" << endl << endl;
 
-	cout << "[b]Depends:[/b] " << depends << endl;
-	cout << "[b]License:[/b] " << license << endl;
+	cout << raw_giturl << "depends.txt][b]Depends:[/b][/url] " << depends << endl;
+	cout << raw_giturl << "LICENSE.txt][b]License:[/b][/url] " << license << endl;
 	cout << "[b]Download:[/b] " << ziplink << ", " << tarlink << endl;
 	cout << "[b]github:[/b] " << giturl << "]" << modname << "[/url]" << endl << endl;
 
--- mdcrtgit.cpp
+++ mdcrtgit.cpp
@@ -5,37 +5,36 @@ using namespace std;
 
 int main() {
 
-	string owner, modname, depends, license, screenshot_link;
+	string owner, modname, screenshot_link;
 
 	cout << "owner = ";
 	cin >> owner;
 	cout << "modname = ";
 	cin >> modname;
-	cout << "depends = ";
-	cin >> depends;
-	cout << "license = ";
-	cin >> license;
 	cout << "screenshot_link = ";
 	cin >> screenshot_link;
 
 	if (owner == "n")
 		owner = "HybridDog";
 
-	string giturl = "[url=https://github.com/" + owner + "/" + modname;
-	string ziplink = giturl + "/archive/master.zip]zip[/url]";
-	string tarlink = giturl + "/archive/master.tar.gz]tar.gz[/url]";
+	if (screenshot_link == "n")
+		screenshot_link = "http://bit.ly/1wOCWpq";
+
+	string giturl = "](https://github.com/" + owner + "/" + modname;
+	string giturl_raw = "](https://raw.githubusercontent.com/" + owner + "/" + modname + "/master/";
+	string ziplink = "[zip" + giturl + "/archive/master.zip)";
+	string tarlink = "[tar.gz" + giturl + "/archive/master.tar.gz)";
 
 
 	cout << "--------------------------" << endl << endl;
 
 	cout << "[Mod] " << modname << " [" << modname << "]" << endl << endl;
 
-	cout << "[b]Depends:[/b] " << depends << endl;
-	cout << "[b]License:[/b] " << license << endl;
-	cout << "[b]Download:[/b] " << ziplink << ", " << tarlink << endl;
-	cout << "[b]github:[/b] " << giturl << "]" << modname << "[/url]" << endl << endl;
+	cout << "**Depends:** see [depends.txt" << giturl_raw << "depends.txt)  " << endl;
+	cout << "**License:** see [LICENSE.txt" << giturl_raw << "LICENSE.txt)  " << endl;
+	cout << "**Download:** " << ziplink << ", " << tarlink << "  " << endl << endl;
 
-	cout << "[img]" << screenshot_link << "[/img]" << endl << endl;
+	cout << "![I'm a screenshot!](" << screenshot_link << ")" << endl << endl;
 
 	cout << "If you got ideas or found bugs, please tell them to me." << endl << endl;
 
